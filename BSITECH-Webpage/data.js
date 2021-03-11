dece04436d34f41528829f3d361483e6f59629eb
Lexer Cerdena <lexercerdena01@gmail.com>
2021-03-11 11:08:50 +0800

    Added Simple Project

---
+++ BSITECH-Webpage/data.js
@@ -0,0 +1,92 @@
+const DATA = { 
+	featured: [
+		{
+			id: 01,
+			image: 'imagesProduct/recipe1.jpg',
+			name: 'Lemony seared t-bone steak with kale saute',
+			price: 159.99,
+			desc: 'Searing is a technique for building flavors. It requires a pan set over high heat to quickly brown the surface of meat and caramelize it (use cast-iron or stainless steel for best result). To get a perfect sear, use a thin coating neutral oil with a high smoke point in the pan and add the meat in batches for optimal flavour and remember, only flip the meat once.'
+		},
+		{
+			id: 02,
+			image: 'imagesProduct/recipe2.jpg',
+			name: 'Braised Beef Short Ribs',
+			price: 159.99,
+			desc: 'Braising uses two cooking methods, requiring both moist and dry heat. Typically you’ll find tough cuts of meats such as chunk, shank or brisket are prepared using this method, but you can also braise root vegetables and winter squash. First the food is seared, and then it’s covered with a braising liquid (broth or wine) and cooked at a very low simmer until incredibly tender.'
+		},
+		{
+			id: 03,
+			image: 'imagesProduct/recipe3.jpg',
+			name: 'Steak with sherried mushrooms',
+			price: 39.99,
+			desc: 'Reducing is the process of boiling and cooking down the liquid in a sauce or a soup reducing its original volume. As the liquid boils, it will evaporate creating an intensely flavored, thick sauce.'
+		},
+		{
+			id: 04,
+			image: 'imagesProduct/recipe4.jpg',
+			name: 'Lemony Rapini',
+			price: 129.99,
+			desc: "Blanching is a technique typically used for cooking vegetables. Vegetables are submerged in a pot of boiling water and cooked for 1-5 minutes depending on the vegetable, for example, baby carrots take five minutes while asparagus will blanch for about three. The vegetables are immediately transferred to a bowl of ice water to stop the cooking process."
+		},
+		{
+			id: 05,
+			image: 'imagesProduct/recipe5.jpg',
+			name: '10-minute homemade chilli oil',
+			price: 249.99,
+			desc: "Infusing is a process of extracting flavor from any aromatic ingredient (such as tea or herbs) by steeping them in a hot liquid (such as water, milk or oil) resulting in a flavorful infusion. These infusions can be used to create flavorful liquids to use as a finishing touch on a dish or to add to sauces and syrups."
+		},
+		{
+			id: 06,
+			image: 'imagesProduct/recipe6.jpg',
+			name: 'White chocolate mousse with cherries',
+			price: 299.99,
+			desc: "Macerating involves soaking fruit (fresh or dried) in a flavored liquid such as vinegar or liquor. It can also be used to describe sprinkling fruit with sugar, which draws out the liquid, creating a syrup. This allows the fruit to soak up the flavor and the liquids, resulting in a softer, juicer fruit."
+		},
+		{
+			id: 07,
+			image: 'imagesProduct/recipe7.jpg',
+			name: 'Hollandaise sauce',
+			price: 39.99,
+			desc: "When liquid and solids separate, a mixture is curdling. In a hot preparations using eggs as an emulsifier or dairy products (custards of hollandaise), the sauce will curdle when 	overheated. To avoid, heat the sauce gently and be careful not to overcook a sauce."
+		},
+		{
+			id: 08,
+			image: 'imagesProduct/recipe8.jpg',
+			name: 'Home made chicken broth',
+			price: 599.99,
+			desc: "When a recipe calls for “skimming the surface” it is referring to removing the layer scum or fat from the surface of liquid (typically found when making jam or stock). This can be done using a slotted spoon (for removing solids) or a regular spoon (for removing liquids)"
+		}
+	],
+
+	categories: [
+		{
+			image: 'magazineImages/magazine1.jpg'
+			// title: 'Musical Toys'
+		},
+		{
+			image: 'magazineImages/magazine2.jpg'
+			// title: 'Infant & Preschool'
+		},
+		{
+			image: 'magazineImages/magazine3.jpg'
+			// title: 'Art & <br>Craft'
+		},
+		{
+			image: 'magazineImages/magazine4.jpg'
+			// title: 'Games & <br>Puzzle'
+		},
+		{
+			image: 'magazineImages/magazine5.jpg'
+			// title: 'Outdoor <br>Toys, Sports <br>& Games'
+		},
+		{
+			image: 'magazineImages/magazine6.jpg'
+			// title: 'Wooden Toys'
+		},
+	],
+
+	figureImage:
+		{
+			image: 'https://cdn11.bigcommerce.com/s-a4k7wcbf6g/stencil/305ebf50-b9d6-0138-b174-0242ac11000f/e/86d849d0-a235-0138-5657-0242ac11000b/img/custom-assets/home/header-accent-top.svg'
+		}
+}
